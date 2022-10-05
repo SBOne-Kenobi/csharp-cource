f66c87601a851c78d47fc50909d34a5974273715
Rustam Sadykov <sbonekenobi@gmail.com>
2022-10-05 12:07:23 +0300

    make Delegates

---
+++ HW4/Delegates/Integrator.cs
@@ -0,0 +1,24 @@
+﻿namespace Delegates;
+
+public delegate double Function(double x);
+
+public class Integrator
+{
+    private const int SegmentCount = 1000000;
+    
+    public static double Integrate(Function f, double a, double b)
+    {
+        var result = 0.0;
+        var stepSize = (b - a) / SegmentCount;
+        var currentX = a;
+        for (var i = 0; i < SegmentCount; i++)
+        {
+            var nextX = currentX + stepSize;
+            var x = (nextX + currentX) / 2;
+            result += f(x) * stepSize;
+            currentX = nextX;
+        }
+
+        return result;
+    }
+}
---
+++ HW4/Delegates/Program.cs
@@ -0,0 +1,6 @@
+﻿using Delegates;
+
+Console.WriteLine(Integrator.Integrate(x => x, 0, 1)); // 0.5
+Console.WriteLine(Integrator.Integrate(Math.Sin, 0, Math.PI)); // 2.0
+Console.WriteLine(Integrator.Integrate(x => 3 * x * x, 0, 1)); // 1.0
+Console.WriteLine(Integrator.Integrate(Math.Exp, 0, Math.Log(3))); // 2.0
