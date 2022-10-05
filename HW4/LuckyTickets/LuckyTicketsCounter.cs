1eab6e3b34c1e2f9d13e87b688b8ea33df45c0b4
Rustam Sadykov <sbonekenobi@gmail.com>
2022-10-05 14:04:35 +0300

    make LuckyTickets

---
+++ HW4/LuckyTickets/LuckyTicketsCounter.cs
@@ -0,0 +1,55 @@
+﻿namespace LuckyTickets;
+
+public class LuckyTicketsCounter
+{
+    private readonly Dictionary<Tuple<int, int>, long> _numberOfTicketsWithLengthAndSum = new();
+
+    private long GetNumberOfTickets(int length, int sum)
+    {
+        var maxSum = 9 * length;
+        if (sum > maxSum || sum < 0)
+        {
+            return 0;
+        }
+
+        switch (length)
+        {
+            case < 0:
+                return 0;
+            case <= 1:
+                return 1;
+        }
+
+        var key = new Tuple<int, int>(length, sum);
+        if (!_numberOfTicketsWithLengthAndSum.ContainsKey(key))
+        {
+            var result = 0L;
+            for (var digit = 0; digit <= 9; digit++)
+            {
+                result += GetNumberOfTickets(length - 1, sum - digit);
+            }
+            _numberOfTicketsWithLengthAndSum[key] = result;
+        }
+
+        return _numberOfTicketsWithLengthAndSum[key];
+    }
+
+    public long LuckyTicket(int digitsNumber)
+    {
+        if (digitsNumber % 2 != 0)
+        {
+            return 0;
+        }
+
+        var halfLength = digitsNumber / 2;
+        var maxSum = 9 * halfLength;
+        var result = 0L;
+        for (var targetSum = 0; targetSum <= maxSum; targetSum++)
+        {
+            var number = GetNumberOfTickets(halfLength, targetSum);
+            result += number * number;
+        }
+
+        return result;
+    }
+}
---
+++ HW4/LuckyTickets/Program.cs
@@ -0,0 +1,22 @@
+﻿using LuckyTickets;
+
+var counter = new LuckyTicketsCounter();
+
+var resultList = new List<Tuple<int, long>>
+{
+    new (2, 10),
+    new (4, 670),
+    new (6, 55252),
+    new (8, 4816030),
+    new (10, 432457640),
+    new (12, 39581170420),
+    new (14, 3671331273480)
+};
+
+foreach (var (digitsNumber, expectedResult) in resultList)
+{
+    var actualResult = counter.LuckyTicket(digitsNumber);
+    Console.WriteLine("LuckyTicket(" + digitsNumber + "):");
+    Console.WriteLine("  Expected: " + expectedResult);
+    Console.WriteLine("  Actual:   " + actualResult);
+}
