ef27eb25ab51c3b139e379795af1e6447bb4ff89
Rustam Sadykov <sbonekenobi@gmail.com>
2022-10-12 15:10:13 +0300

    make 5.2

---
+++ HW5/PseudoStack/PseudoStack.cs
@@ -0,0 +1,45 @@
+﻿namespace PseudoStack;
+
+public class PseudoStack<T>
+{
+    private readonly List<Stack<T>> _stacks = new ();
+    private readonly int _maxInStack;
+
+    public PseudoStack() : this(10)
+    {
+    }
+
+    public PseudoStack(int maxInStack)
+    {
+        _maxInStack = maxInStack;
+    }
+
+    public void Push(T value)
+    {
+        var targetStack = _stacks.LastOrDefault();
+        var addNew = false;
+        if (targetStack == null || targetStack.Count == _maxInStack)
+        {
+            targetStack = new Stack<T>(_maxInStack);
+            addNew = true;
+        }
+
+        targetStack.Push(value);
+        
+        if (addNew)
+        {
+            _stacks.Add(targetStack);
+        }
+    }
+
+    public T Pop()
+    {
+        var targetStack = _stacks.Last();
+        var result = targetStack.Pop();
+        if (targetStack.Count == 0)
+        {
+            _stacks.RemoveAt(_stacks.Count - 1);
+        }
+        return result;
+    }
+}
---
+++ HW5/UnitTests/PseudoStackTest.cs
@@ -0,0 +1,34 @@
+﻿namespace UnitTests;
+
+public class PseudoStackTest
+{
+    [Fact]
+    public void Test()
+    {
+        var trueStack = new Stack<int>();
+        var pseudoStack = new PseudoStack.PseudoStack<int>(3);
+
+        var toAdd = new List<int>(Enumerable.Range(1, 10));
+        foreach (var i in toAdd)
+        {
+            trueStack.Push(i);
+            pseudoStack.Push(i);
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            Assert.Equal(trueStack.Pop(), pseudoStack.Pop());
+        }
+        
+        foreach (var i in toAdd)
+        {
+            trueStack.Push(i);
+            pseudoStack.Push(i);
+        }
+
+        for (int i = 0; i < trueStack.Count; i++)
+        {
+            Assert.Equal(trueStack.Pop(), pseudoStack.Pop());
+        }
+    }
+}
