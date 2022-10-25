abcfe10540fbd3cb9f7a7d1fa8b8bcf7f2f6c136
Rustam Sadykov <sbonekenobi@gmail.com>
2022-10-25 19:55:29 +0300

    make 7.2

---
+++ HW7/FindingElements/FindUtil.cs
@@ -0,0 +1,15 @@
+﻿namespace FindingElements;
+
+public interface IWithName
+{
+    string GetName();
+}
+
+public static class FindUtil
+{
+    public static IEnumerable<T> FindStrangeElements<T>(this IEnumerable<T> names) where T : IWithName
+    {
+        return names
+            .Where((item, idx) => item.GetName().Length > idx);
+    }
+}
---
+++ HW7/UnitTests/FindingElementsTest.cs
@@ -0,0 +1,45 @@
+﻿using FindingElements;
+
+namespace UnitTests;
+
+public class FindingElementsTest
+{
+    class WithName : IWithName
+    {
+        private readonly string _name;
+
+        public WithName(string name)
+        {
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+    
+    [Fact]
+    public void FindStrangeElementsTest()
+    {
+        var names = new List<string>
+        {
+            "John",
+            "N",
+            "Kate",
+            "Li",
+            "Alexandr"
+        };
+        var sample = names.Select(name => new WithName(name));
+        var expected = new List<string>
+        {
+            "John",
+            "Kate",
+            "Alexandr"
+        };
+        var actual = sample
+            .FindStrangeElements()
+            .Select(item => item.GetName());
+        Assert.Equal(expected, actual);
+    }
+}
