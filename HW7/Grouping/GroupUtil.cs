8793fe5c8027deaea548da449b007a0e52403da5
Rustam Sadykov <sbonekenobi@gmail.com>
2022-10-25 20:38:26 +0300

    make 7.3

---
+++ HW7/Grouping/GroupUtil.cs
@@ -0,0 +1,23 @@
+﻿namespace Grouping;
+
+public static class GroupUtil
+{
+    public static string GetInfoAboutSentence(this string sentence)
+    {
+        var words = sentence
+            .Select(c => Char.IsPunctuation(c) ? ' ' : c)
+            .Aggregate("", (acc, c) => acc + c)
+            .Split()
+            .Where(word => word.Length > 0);
+        
+        var groupsOfWords = words
+            .GroupBy(word => word.Length)
+            .OrderByDescending(group => group.Key);
+
+        return groupsOfWords
+            .Select((group, idx) => 
+                $"Group {idx + 1}. Length {group.Key}. Count {group.Count()}" +
+                group.Aggregate("\n", (acc, word) => acc + word + "\n")
+            ).Aggregate((acc, groupInfo) => acc + "\n" + groupInfo);
+    }
+}
---
+++ HW7/UnitTests/GroupingTest.cs
@@ -0,0 +1,38 @@
+﻿using Grouping;
+
+namespace UnitTests;
+
+public class GroupingTest
+{
+    [Fact]
+    public void GetInfoAboutSentenceTest()
+    {
+        var sentence = "Это что же получается:::ходишь, ходишь в школу,а потом бац - вторая смена";
+        var expected = "Group 1. Length 10. Count 1\n" +
+                       "получается\n" +
+                       "\n" +
+                       "Group 2. Length 6. Count 3\n" +
+                       "ходишь\n" +
+                       "ходишь\n" +
+                       "вторая\n" +
+                       "\n" +
+                       "Group 3. Length 5. Count 3\n" +
+                       "школу\n" +
+                       "потом\n" +
+                       "смена\n" +
+                       "\n" +
+                       "Group 4. Length 3. Count 3\n" +
+                       "Это\n" +
+                       "что\n" +
+                       "бац\n" +
+                       "\n" +
+                       "Group 5. Length 2. Count 1\n" +
+                       "же\n" +
+                       "\n" +
+                       "Group 6. Length 1. Count 2\n" +
+                       "в\n" +
+                       "а\n";
+        var actual = sentence.GetInfoAboutSentence();
+        Assert.Equal(expected, actual);
+    }
+}
