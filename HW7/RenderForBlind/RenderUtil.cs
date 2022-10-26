01434a7575522a60e608c9f1418e7d7ce2d0254b
Rustam Sadykov <sbonekenobi@gmail.com>
2022-10-26 12:32:46 +0300

    make 7.4

---
+++ HW7/RenderForBlind/RenderUtil.cs
@@ -0,0 +1,14 @@
+﻿namespace RenderForBlind;
+
+public static class RenderUtil
+{
+    public static string TextRenderForBlind(this string text, Dictionary<string, string> dictionary, int maxInPage)
+    {
+        return text
+            .Split()
+            .Select(word => dictionary[word.ToLower()].ToUpper())
+            .Chunk(maxInPage)
+            .Select(page => string.Join(" ", page))
+            .Aggregate((acc, page) => acc + "\n" + page);
+    }
+}
---
+++ HW7/UnitTests/RenderForBlindTest.cs
@@ -0,0 +1,28 @@
+﻿using RenderForBlind;
+
+namespace UnitTests;
+
+public class RenderForBlindTest
+{
+    [Fact]
+    public void TextRenderForBlindTest()
+    {
+        var text = "This dog eats too much vegetables after lunch";
+        var dictionary = new Dictionary<string, string>
+        {
+            {"this", "эта"},
+            {"dog", "собака"},
+            {"eats", "ест"},
+            {"too", "слишком"},
+            {"much", "много"},
+            {"vegetables", "овощей"},
+            {"after", "после"},
+            {"lunch", "обеда"}
+        };
+        var expected = "ЭТА СОБАКА ЕСТ\n" +
+                     "СЛИШКОМ МНОГО ОВОЩЕЙ\n" +
+                     "ПОСЛЕ ОБЕДА";
+        var actual = text.TextRenderForBlind(dictionary, 3);
+        Assert.Equal(expected, actual);
+    }
+}
