5067a6f0520b5f9478bc28ee876d8d4ee824bb78
Rustam Sadykov <sbonekenobi@gmail.com>
2022-10-26 12:47:53 +0300

    make 7.5

---
+++ HW7/UnitTests/WordBlocksTest.cs
@@ -0,0 +1,29 @@
+﻿using WordBlocks;
+
+namespace UnitTests;
+
+public class WordBlocksTest
+{
+    [Fact]
+    public void BucketizeTest()
+    {
+        var text = "она  продает морские раковины у моря";
+        var expected = new[]
+        {
+            "она  продает",
+            "морские раковины",
+            "у моря"
+        };
+        var actual = text.Bucketize(16);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void BucketizeFailedTest()
+    {
+        
+        var text = "она  продает морские раковины у моря";
+        var actual = text.Bucketize(4);
+        Assert.Empty(actual);
+    }
+}
---
+++ HW7/WordBlocks/WordBlockUtil.cs
@@ -0,0 +1,31 @@
+﻿namespace WordBlocks;
+
+public static class WordBlockUtil
+{
+    public static string[] Bucketize(this string text, int bucketSize)
+    {
+        var words = text.Split();
+        if (words.Any(word => word.Length > bucketSize))
+        {
+            return Array.Empty<string>();
+        }
+
+        return words
+            .Aggregate(new List<string>(), (acc, word) =>
+            {
+                if (acc.Count > 0 && acc[^1].Length + word.Length + 1 <= bucketSize)
+                {
+                    acc[^1] += " " + word;
+                }
+                else
+                {
+                    if (acc.Count > 0)
+                    {
+                        acc[^1] = acc[^1].Trim();
+                    }
+                    acc.Add(word);
+                }
+                return acc;
+            }).ToArray();
+    }
+}
